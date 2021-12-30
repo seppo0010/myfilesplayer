5d7582f9a37c1f77a092f20b01bb1fddb79a3c20
Sebastian Waisbrot <seppo0010@gmail.com>
2021-12-30 01:24:28 -0300

    Write open subtitles and episode metadata

--- encoder/src/index.ts
+++ encoder/src/index.ts
@@ -1,27 +1,52 @@
 #!/usr/bin/env ts-node
-import find from 'find';
 import * as child_process from 'child_process';
 import * as fs from 'fs';
 import * as path from 'path';
 
+import find from 'find';
+import OS from 'opensubtitles-api';
+import * as dotenv from 'dotenv';
+import episodeParser from 'episode-parser';
+
+dotenv.config()
+
 const regex = /\.(avi|mp4|mkv)$/;
 const source = process.argv[2];
 const target = process.argv[3];
-find.file(regex, source, (files) => {
-  files.forEach((f) => {
-    child_process.spawnSync(
-      'ffmpeg',
-      [
-        '-i',
-        f,
-        path.join(
-          target,
-          path.basename(f.replace(regex, '.webm')),
-        ),
-      ],
-      { stdio: 'pipe' },
-    )
-    console.log(f, targetPath)
-  })
+const OpenSubtitles = new OS({
+    useragent: 'myfilesplayer',
+    username:  process.env.OSDB_LOGIN,
+    password:  process.env.OSDB_PASSWORD,
+    ssl: true,
 });
+(async function () {
+  find.file(regex, source, (files) => {
+    files.forEach(async (f) => {
+      console.log('Processing', f);
+      const episode = episodeParser(path.basename(f));
+      const opensubtitles = await OpenSubtitles.hash(f);
+      child_process.spawnSync(
+        'ffmpeg',
+        [
+          '-i',
+          f,
+          '-crf',
+          '0',
+          path.join(
+            target,
+            path.basename(f.replace(regex, '.webm')),
+          ),
+        ],
+        { stdio: 'pipe' },
+      )
+      fs.writeFileSync(
+          path.join(
+            target,
+            path.basename(f.replace(regex, '.json')),
+          ),
+          JSON.stringify({ opensubtitles, episode }),
+      );
+    })
+  });
+})();
 export {};
