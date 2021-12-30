a15d4704c97c1cbf32a68af92d2d960229192984
Sebastian Waisbrot <seppo0010@gmail.com>
2021-12-30 02:56:09 -0300

    Search for subtitles

--- webserver/src/index.ts
+++ webserver/src/index.ts
@@ -1,10 +1,18 @@
 import * as fs from 'fs';
+import * as path from 'path';
+import * as https from 'https';
 
 import express from 'express';
+import * as dotenv from 'dotenv';
+import OS from 'opensubtitles.com';
+
+dotenv.config()
 
 const app = express();
 const port = process.env.PORT || 3000;
 const videosPath = process.argv[2];
+const os_api_key = process.env.OS_API_KEY;
+const os = new OS({apikey: os_api_key});
 
 app.get('/videos.json', async (req, res) => {
   const files = Array.prototype.slice.call(await fs.promises
@@ -16,6 +24,18 @@ app.get('/videos.json', async (req, res) => {
   res.json(files);
 })
 app.use('/videos', express.static(videosPath));
+app.get('/subtitles/:video', async (req, res) => {
+  const fileData = JSON.parse(await fs.promises.readFile(path.join(
+    videosPath,
+    `${req.params.video}.json`
+  ), 'utf-8'));
+  const data = await os.subtitles({
+    query: fileData.episode.show,
+    season_number: fileData.episode.season,
+    episode_number: fileData.episode.episode,
+  })
+  res.json(data);
+});
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
