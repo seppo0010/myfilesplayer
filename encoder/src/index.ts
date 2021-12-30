7a9a9cb1a4af504095a56f6299b8370f3feebb9a
Sebastian Waisbrot <seppo0010@gmail.com>
2021-12-30 08:20:33 -0300

    ffmpeg output to console output

--- encoder/src/index.ts
+++ encoder/src/index.ts
@@ -1,8 +1,8 @@
 #!/usr/bin/env ts-node
-import * as child_process from 'child_process';
 import * as fs from 'fs';
 import * as path from 'path';
 
+import { execa } from 'execa';
 import find from 'find';
 import OS from 'opensubtitles-api';
 import * as dotenv from 'dotenv';
@@ -19,34 +19,36 @@ const OpenSubtitles = new OS({
     password:  process.env.OSDB_PASSWORD,
     ssl: true,
 });
-(async function () {
-  find.file(regex, source, (files: string[]) => {
-    files.forEach(async (f: string) => {
-      console.log('Processing', f);
 
-      const webmPath = path.join(target, path.basename(f.replace(regex, '.webm')));
-      if (fs.existsSync(webmPath)) {
-        console.log('Skipping webm', f);
-      } else {
-        child_process.spawnSync('ffmpeg', ['-i', f, '-crf', '0', webmPath], { stdio: 'pipe' });
-      }
+find.file(regex, source, async (files: string[]) => {
+  for (let f of files) {
+    console.log('Processing', f);
 
-      const thumbnailPath = path.join(target, path.basename(f.replace(regex, '.jpg')));
-      if (fs.existsSync(thumbnailPath)) {
-        console.log('Skipping thumbnail', f);
-      } else {
-        child_process.spawnSync('ffmpeg', ['-ss', '200', '-i', f, '-vframes', '1', '-q:v', '2', '-s', '400x200', thumbnailPath], { stdio: 'pipe' });
-      }
+    const webmPath = path.join(target, path.basename(f.replace(regex, '.webm')));
+    if (fs.existsSync(webmPath)) {
+      console.log('Skipping webm', f);
+    } else {
+      const sh = execa('ffmpeg', ['-i', f, '-crf', '0', webmPath])
+      sh.stdout?.pipe(process.stdout)
+      sh.stderr?.pipe(process.stderr);
+      await sh
+    }
 
-      const jsonPath = path.join(target, path.basename(f.replace(regex, '.json')));
-      if (fs.existsSync(jsonPath)) {
-        console.log('Skipping json', f);
-      } else {
-        const episode = episodeParser(path.basename(f));
-        const opensubtitles = await OpenSubtitles.hash(f);
-        fs.writeFileSync(jsonPath, JSON.stringify({ opensubtitles, episode }));
-      }
-    })
-  });
-})();
-export {};
+    const thumbnailPath = path.join(target, path.basename(f.replace(regex, '.jpg')));
+    if (fs.existsSync(thumbnailPath)) {
+      console.log('Skipping thumbnail', f);
+    } else {
+      await execa('ffmpeg', ['-ss', '200', '-i', f, '-vframes', '1', '-q:v', '2', '-s', '400x200', thumbnailPath]);
+    }
+
+    const jsonPath = path.join(target, path.basename(f.replace(regex, '.json')));
+    if (fs.existsSync(jsonPath)) {
+      console.log('Skipping json', f);
+    } else {
+      const episode = episodeParser(path.basename(f));
+      const opensubtitles = await OpenSubtitles.hash(f);
+      fs.writeFileSync(jsonPath, JSON.stringify({ opensubtitles, episode }));
+    }
+  }
+});
+export default {};
