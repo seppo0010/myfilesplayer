76e30626d9d3ec52ee86367db8b158758c01e14f
Sebastian Waisbrot <seppo0010@gmail.com>
2021-12-30 02:10:08 -0300

    List videos

--- webserver/src/index.ts
+++ webserver/src/index.ts
@@ -1,11 +1,21 @@
+import * as fs from 'fs';
+
 import express from 'express';
 
 const app = express();
 const port = process.env.PORT || 3000;
+const videosPath = process.argv[2];
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/videos.json', async (req, res) => {
+  const files = Array.prototype.slice.call(await fs.promises
+                                           .readdir(videosPath))
+    .filter((f) => f.endsWith('.json'))
+    .map((f) => f.substr(0, f.length-5));
+  files.sort();
+  res.type('application/json');
+  res.json(files);
 })
+app.use('/videos', express.static(videosPath));
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
