8afc950d3590775c78e9eccd8592690d32190e32
Sebastian Waisbrot <seppo0010@gmail.com>
2021-12-30 08:52:39 -0300

    Move api endpoints to api and remove debug link

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function App() {
+  const loading = useState(false);
+  const loadedVideos = useState(false);
+  const videos = useState([]);
+  useEffect(() => {
+    if (loading || loadedVideos) return;
+    setLoading(true);
+    fetch('/api/videos.json')
+  });
+
   return (
     <div>
     </div>
--- webserver/src/index.ts
+++ webserver/src/index.ts
@@ -18,7 +18,7 @@ os.login({
   password: process.env.OS_PASSWORD,
 });
 
-app.get('/videos.json', async (req, res) => {
+app.get('/api/videos.json', async (req, res) => {
   const files = Array.prototype.slice.call(await Promise.all(
     Array.prototype.slice.call(await fs.promises
                                            .readdir(videosPath))
@@ -30,7 +30,7 @@ app.get('/videos.json', async (req, res) => {
   res.json(files);
 })
 app.use('/videos', express.static(videosPath));
-app.get('/subtitles/:video', async (req, res) => {
+app.get('/api/subtitles/:video', async (req, res) => {
   const fileData = JSON.parse(await fs.promises.readFile(path.join(
     videosPath,
     `${req.params.video}.json`
@@ -42,11 +42,11 @@ app.get('/subtitles/:video', async (req, res) => {
   })
   res.json(data);
 });
-app.get('/subtitle/:file_id', async (req, res) => {
+app.get('/api/subtitle/:file_id', async (req, res) => {
   const data = await os.download({
     file_id: req.params.file_id,
   });
-  const response = await fetch('https://www.opensubtitles.com/download/2E142134EE2AAAC1B52E2AFB17B04669452D4DD86144FC4C0D92EAB5198D32055B50267DB4530855225D56F7BB508A7027BB2790CACA0C61235D379A2B60E83233450529EDC53307669E78D0117DD9FD17D43B81608B5030ADC10A7FE9EE1F0ECB65ADCE23EEE39965F8953E0AD653BBB22672AD3868820371C81F10F7ED1DB46423AEA78E04A29D9163B1C446D34A27493A96195F2562E17CB43CE8FCECFAFEBC7FD7638FDFF32394F9B4796BB5B5FD4CD34F88E4317A1426FB9C5215083B0EB60357B092A1BDBADEFA0A69339D3581C65D4BF6F02EFA9B9622F40CF0D5BB22A1F9A886E5B13A31F39CB482B4697CEA4A3BC4484786E6163C51EDD34C0255C32C44B312CAFE8B085435397EE81F37A20DC6E38E67A8721751A938D41E199C1886B7246CF69BE0C7455353E52FF85B42/subfile/The%20West%20Wing%20-%2001x02%20-%20Post%20Hoc,%20Ergo%20Propter%20Hoc.srt');
+  const response = await fetch(data.link);
   res.send(await response.text());
 });
 
