70d2bc1b4d673544687db04470e1a76d855f56fa
Sebastian Waisbrot <seppo0010@gmail.com>
2022-01-02 14:26:05 -0300

    Dockerize database

--- webserver/src/db.ts
+++ webserver/src/db.ts
@@ -4,17 +4,19 @@ import { Pool } from 'pg';
 
 const getFileContents = (path: string): string => {
   try {
-    return fs.readFileSync('/var/run/secrets/pg_hostname', 'utf-8').trim()
+    return fs.readFileSync(path, 'utf-8').trim()
   } catch (e) {
     return '';
   }
 }
+
+
 const pool = new Pool({
-  host: process.env.PG_HOSTNAME || getFileContents('/var/run/secrets/pg_hostname'),
-  user: process.env.PG_USERNAME || getFileContents('/var/run/secrets/pg_username'),
-  database: process.env.PG_DATABASE || getFileContents('/var/run/secrets/pg_database'),
-  password: process.env.PG_PASSWORD || getFileContents('/var/run/secrets/pg_password'),
-  port: parseInt(process.env.PG_PORT || getFileContents('/var/run/secrets/pg_port'), 10),
+  host: process.env.POSTGRES_HOST || getFileContents('/run/secrets/postgres-hostname') || 'localhost',
+  user: process.env.POSTGRES_USER || getFileContents('/run/secrets/postgres-username'),
+  database: process.env.POSTGRES_DB || getFileContents('/run/secrets/postgres-database'),
+  password: process.env.POSTGRES_PASSWORD || getFileContents('/run/secrets/postgres-password'),
+  port: parseInt(process.env.POSTGRES_PORT || getFileContents('/run/secrets/postgres-port') || '5432', 10),
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
